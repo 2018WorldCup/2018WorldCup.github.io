7cfa6c557a5065263c90954f49bb223e4fb16d6b
pingping <pingping_0113@126.com>
2018-03-22 21:59:38 +0800

    初始化个人主页仓库

--- js/nav.js
+++ js/nav.js
@@ -1,40 +1,30 @@
 nav = {
-    createNav:function(divID){
+    createNav:function(divID, indexID){
 	    var div1 = document.getElementById(divID);
+		var liList = ["", "", "", "", ""];
+		if(indexID != -1){
+			liList[indexID] = "active";
+		}
 		var code = '<div class="col-md-12 column">'
-					+ '<nav class="navbar navbar-default navbar-fixed-top" role="navigation" style="background-color:#E7E7E7;"><!-- style="padding-left:100px"-->'
-					+ '	<div class="navbar-header">'
-					+ '		 <a class="navbar-brand" href="/" style="cursor:pointer;"><img alt="Brand" height="40px" src="images/2018.jpg"/></a> <!--60-->'
-					+ '	</div>'
-					+ '	<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1" style="margin-top:8px;"><!--20-->'
-					+ '		<ul class="nav navbar-nav">'
-					+ '			<li>'
-					+ '				 <a href="/" style="cursor:pointer;font-size:18px;height:25px;line-height:25px;"><img alt="Brand" height="15px" src="images/football.jpg"/><strong>World Cup</strong></a><!--style=" color:#666; font-size:12px;"-->'
-					+ '			</li>'
-					+ '			<li>'
-					+ '				 <a href="myteam.html" style="cursor:pointer;font-size:18px;height:25px;line-height:25px;"><img alt="Brand" height="15px" src="images/football.jpg"/><strong>My Team</strong></a>'
-					+ '			</li>'
-					+ '			<li>'
-					+ '				 <a href="tradding.html" style="cursor:pointer;font-size:18px;height:25px;line-height:25px;"><img alt="Brand" height="15px" src="images/football.jpg"/><strong>Trading Record</strong></a><!--Trading Record-->'
-					+ '			</li>'
-					+ '			<li>'
-					+ '				 <a href="FAQ.html" style="cursor:pointer;font-size:18px;height:25px;line-height:25px;"><img alt="Brand" height="15px" src="images/football.jpg"/><strong>FAQ</strong></a><!--Trading Record-->'
-					+ '			</li>'
-					+ '			<li>'
-					+ '				 <a href="present.html" style="cursor:pointer;font-size:18px;height:25px;line-height:25px;"><img alt="Brand" height="15px" src="images/football.jpg"/><strong>Crowdfunding</strong></a><!--Trading Record-->'
-					+ '			</li>'
-					+ '		</ul>'
-					+ '		<ul class="nav navbar-nav navbar-right">'
-					+ '			<li>'
-					+ '				 <a href="" style="font-size:23px;height:25px;line-height:25px;">Bonus Pool: <strong><span class="pools" style="color:#FF0000">0</span></strong> ETH</a> <!--The next dividend is: <span class="pooltime">1</span>s</a>-->'
-					+ '			</li><!--<img alt="Brand" height="30px" src="images/ether.png"/>-->'
-					+ '			<li>'
-					+ '				 <a onclick="App.geiPools();" style="cursor:pointer;font-size:20px;height:25px;line-height:25px;"><img alt="Brand" height="25px" src="images/pool.jpg"/></a>'
-					+ '			</li>'
-					+ '		</ul>'
-					+ '	</div>'
-					+ '</nav>'
-				    + '</div>';
+				+  '<nav class="navbar navbar-default navbar-fixed-top" role="navigation">'
+				+  '		<div class="navbar-header">'
+				+  '			 <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"> <span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button> <a class="navbar-brand" href="/"><img alt="Brand" height="30px" src="images/2018.jpg"/></a>'
+				+  '		</div>'
+				+  '		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">'
+				+  '			<ul class="nav navbar-nav">'
+				+  '				<li class='+liList[0]+'><a href="/" style="font-size:16px;"><img height="15px" src="images/football.jpg"/><strong>World Cup</strong></a></li>'
+				+  '				<li class='+liList[1]+'><a href="myteam.html" style="font-size:16px;"><img height="15px" src="images/football.jpg"/><strong>My Team</strong></a></li>'
+				+  '				<li class='+liList[2]+'><a href="tradding.html" style="font-size:16px;"><img height="15px" src="images/football.jpg"/><strong>Trading Record</strong></a></li>'
+				+  '				<li class='+liList[3]+'><a href="FAQ.html" style="font-size:16px;"><img height="15px" src="images/football.jpg"/><strong>FAQ</strong></a></li>'
+				+  '				<li class='+liList[4]+'><a href="present.html" style="font-size:16px;"><img height="15px" src="images/football.jpg"/><strong>Crowdfunding</strong></a></li>'
+				+  '			</ul>'
+				+  '			<ul class="nav navbar-nav navbar-right">'
+				+  '				<li><a href="" style="font-size:23px;">Bonus Pool: <strong><span class="pools" style="color:#FF0000">0</span></strong> ETH</a></li>'
+				+  '				<li><a onclick="App.geiPools();" style="cursor:pointer;"><img alt="Brand" height="18px" src="images/pool.jpg"/></a></li>'
+				+  '			</ul>'
+				+  '		</div>'
+				+  '	</nav>'
+				+  '</div>';
 		div1.innerHTML = code;
     }
 };
